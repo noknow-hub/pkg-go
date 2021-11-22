ede3540dddc5326d15c997d22291ef76e38323eb
Kakeru Sato <sato.kakeru3@gmail.com>
2021-11-22 20:46:14 +0400

    pre-release

--- vendor/dataforseo/dataforseo.go
+++ vendor/dataforseo/dataforseo.go
@@ -3,23 +3,4 @@
 //////////////////////////////////////////////////////////////////////
 package dataforseo
 
-import (
-    myAuthentication "github.com/noknow-hub/pkg-go/vendor/dataforseo/client/authentication"
-    myConstant "github.com/noknow-hub/pkg-go/vendor/dataforseo/constant"
 
-)
-
-
-//////////////////////////////////////////////////////////////////////
-// New OrganicTaskPostClient object.
-//////////////////////////////////////////////////////////////////////
-func NewSerpGoogleOrganicTaskPostClient(login, password string, isSandbox bool) *OrganicTaskPostClient {
-    endpointUrl := myConstant.ENDPOINT_SERP_GOOGLE_ORGANIC_TASK_POST_V3
-    if isSandbox {
-        endpointUrl = myConstant.ENDPOINT_SERP_GOOGLE_ORGANIC_TASK_POST_V3_SANDBOX
-    }
-    return &OrganicTaskPostClient{
-        Authentication: myAuthentication.NewAuthentication(login, password),
-        EndpointUrl: endpointUrl,
-    }
-}

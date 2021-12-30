f3699369dac0ca88189149e929b2c9b68a19734c
Kakeru Sato <sato.kakeru3@gmail.com>
2021-12-30 21:46:22 +0400

    pre-release

--- db/mysql/model/image/go.mod
+++ db/mysql/model/image/go.mod
@@ -4,6 +4,5 @@ go 1.17
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0
-	github.com/noknow-hub/pkg-go/db/mysql/model/util v0.0.0-20211227095149-92a50d8e7492
-	github.com/noknow-hub/pkg-go/db/mysql/query v0.0.0-20211227095149-92a50d8e7492
+	github.com/noknow-hub/pkg-go/db/mysql v0.1.2
 )
--- db/mysql/model/image/util.go
+++ db/mysql/model/image/util.go
@@ -6,7 +6,7 @@ package image
 import (
     "errors"
     "strings"
-    myModelUtil "github.com/noknow-hub/pkg-go/db/mysql/model/util"
+    myUtil "github.com/noknow-hub/pkg-go/db/mysql/util"
     myQuery "github.com/noknow-hub/pkg-go/db/mysql/query"
 )
 
@@ -20,73 +20,73 @@ func scanImage(row *myQuery.Row, image *Image) error {
         col.Name = s[len(s)-1]
 
         if col.Name == COL_ID {
-            if val, err := myModelUtil.ConvertInterfaceToString(col.Value); err != nil {
+            if val, err := myUtil.ConvertInterfaceToString(col.Value); err != nil {
                 return err
             } else {
                 image.Id = val
             }
         } else if col.Name == COL_URL {
-            if val, err := myModelUtil.ConvertInterfaceToString(col.Value); err != nil {
+            if val, err := myUtil.ConvertInterfaceToString(col.Value); err != nil {
                 return err
             } else {
                 image.Url = val
             }
         } else if col.Name == COL_NAME {
-            if val, err := myModelUtil.ConvertInterfaceToString(col.Value); err != nil {
+            if val, err := myUtil.ConvertInterfaceToString(col.Value); err != nil {
                 return err
             } else {
                 image.Name = val
             }
         } else if col.Name == COL_SIZE {
-            if val, err := myModelUtil.ConvertInterfaceToString(col.Value); err != nil {
+            if val, err := myUtil.ConvertInterfaceToString(col.Value); err != nil {
                 return err
             } else {
                 image.Size = val
             }
         } else if col.Name == COL_PATH {
-            if val, err := myModelUtil.ConvertInterfaceToString(col.Value); err != nil {
+            if val, err := myUtil.ConvertInterfaceToString(col.Value); err != nil {
                 return err
             } else {
                 image.Path = val
             }
         } else if col.Name == COL_ALT {
-            if val, err := myModelUtil.ConvertInterfaceToString(col.Value); err != nil {
+            if val, err := myUtil.ConvertInterfaceToString(col.Value); err != nil {
                 return err
             } else {
                 image.Alt = val
             }
         } else if col.Name == COL_TYPE {
-            if val, err := myModelUtil.ConvertInterfaceToString(col.Value); err != nil {
+            if val, err := myUtil.ConvertInterfaceToString(col.Value); err != nil {
                 return err
             } else {
                 image.Type = val
             }
         } else if col.Name == COL_MIME_TYPE {
-            if val, err := myModelUtil.ConvertInterfaceToString(col.Value); err != nil {
+            if val, err := myUtil.ConvertInterfaceToString(col.Value); err != nil {
                 return err
             } else {
                 image.MimeType = val
             }
         } else if col.Name == COL_LABEL {
-            if val, err := myModelUtil.ConvertInterfaceToString(col.Value); err != nil {
+            if val, err := myUtil.ConvertInterfaceToString(col.Value); err != nil {
                 return err
             } else {
                 image.Label = val
             }
         } else if col.Name == COL_LINK {
-            if val, err := myModelUtil.ConvertInterfaceToString(col.Value); err != nil {
+            if val, err := myUtil.ConvertInterfaceToString(col.Value); err != nil {
                 return err
             } else {
                 image.Link = val
             }
         } else if col.Name == COL_CREATED_AT {
-            if val, err := myModelUtil.ConvertInterfaceToTime(col.Value); err != nil {
+            if val, err := myUtil.ConvertInterfaceToTime(col.Value); err != nil {
                 return err
             } else {
                 image.CreatedAt = val
             }
         } else if col.Name == COL_UPDATED_AT {
-            if val, err := myModelUtil.ConvertInterfaceToTime(col.Value); err != nil {
+            if val, err := myUtil.ConvertInterfaceToTime(col.Value); err != nil {
                 return err
             } else {
                 image.UpdatedAt = val

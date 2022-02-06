ba32472c38d3fc7210079ae8d1177ed577fec66b
Kakeru Sato <sato.kakeru3@gmail.com>
2022-02-06 23:23:42 +0400

    pre

--- dataforseo/response/v3/keywords_data/google_ads/search_volume/task_get/task_get.go
+++ dataforseo/response/v3/keywords_data/google_ads/search_volume/task_get/task_get.go
@@ -21,19 +21,19 @@ type Task struct {
 type Result struct {
     Keyword string                      `json:"keyword"`
     Spell string                        `json:"spell"`
-    LocationCode int                    `json:"location_code"`
+    LocationCode int64                  `json:"location_code"`
     LanguageCode string                 `json:"language_code"`
     SearchPartners bool                 `json:"search_partners"`
     Competition string                  `json:"competition"`
-    CompetitionIndex int                `json:"competition_index"`
-    SearchVolume int                    `json:"search_volume"`
+    CompetitionIndex int64              `json:"competition_index"`
+    SearchVolume int64                  `json:"search_volume"`
     LowTopOfPageBid float64             `json:"low_top_of_page_bid"`
     HighTopOfPageBid float64            `json:"high_top_of_page_bid"`
     MonthlySearches []*MonthlySearch    `json:"monthly_searches"`
 }
 
 type MonthlySearch struct {
-    Year int            `json:"year"`
-    Month int           `json:"month"`
-    SearchVolume int    `json:"search_volume"`
+    Year int64            `json:"year"`
+    Month int64           `json:"month"`
+    SearchVolume int64    `json:"search_volume"`
 }

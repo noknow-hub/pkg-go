1a3d46c04925d33f5290043083a1ef9c23a83c14
Kakeru Sato <sato.kakeru3@gmail.com>
2022-02-16 03:14:19 +0400

    pre

--- dataforseo/response/v3/keywords_data/google_ads/search_volume/task_get/task_get.go
+++ dataforseo/response/v3/keywords_data/google_ads/search_volume/task_get/task_get.go
@@ -26,6 +26,7 @@ type Result struct {
     SearchPartners bool                 `json:"search_partners"`
     Competition string                  `json:"competition"`
     CompetitionIndex int64              `json:"competition_index"`
+    CompetitionIndexed int64            `json:"competition_indexed"`
     SearchVolume int64                  `json:"search_volume"`
     LowTopOfPageBid float64             `json:"low_top_of_page_bid"`
     HighTopOfPageBid float64            `json:"high_top_of_page_bid"`

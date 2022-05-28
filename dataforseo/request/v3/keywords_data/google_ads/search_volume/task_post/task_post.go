5cd2df7a267a7b4cf711f9d46096df3be328aee5
Kakeru Sato <sato.kakeru3@gmail.com>
2022-05-28 21:02:14 +0300

    dev

--- dataforseo/request/v3/keywords_data/google_ads/search_volume/task_post/task_post.go
+++ dataforseo/request/v3/keywords_data/google_ads/search_volume/task_post/task_post.go
@@ -22,7 +22,7 @@ var (
     NumOfApiCalls = 0
     LastCalledAt time.Time
     ApiCallQueues []time.Time
-    Reg = regexp.MustCompile("[!！@＠%％^＾()（）=＝{};；：~〜～`｀<>＜＞?？\\|｜,、，。…．Ⅱ⇨:ｦ-ﾝ○※＊「」｣［］【】｛｝‘’“”〈〉〔〕《》*🟡×『』－]")
+    Reg = regexp.MustCompile("[!！@＠%％^＾()（）=＝{};；：~〜～`｀<>＜＞?？\\|｜,、，。…．Ⅱ⇨:ｦ-ﾝ○※＊「」｣［］【】｛｝‘’“”〈〉〔〕《》*🟡×『』－〇]")
 )
 
 type Client struct {

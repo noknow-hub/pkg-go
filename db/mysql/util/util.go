b841fa2f1dbe29316fe3a484daeb66d9efd8651f
Kakeru Sato <sato.kakeru3@gmail.com>
2023-12-31 17:45:30 +0900

    dev

--- db/mysql/util/util.go
+++ db/mysql/util/util.go
@@ -14,6 +14,9 @@ import (
 // Convert interface to bool.
 //////////////////////////////////////////////////////////////////////
 func ConvertInterfaceToBool(value interface{}) (bool, error) {
+    if value == nil {
+        return false, nil
+    }
     nb := &sql.NullBool{}
     if err := nb.Scan(value); err != nil {
         return false, err
@@ -26,6 +29,9 @@ func ConvertInterfaceToBool(value interface{}) (bool, error) {
 // Convert interface to float64.
 //////////////////////////////////////////////////////////////////////
 func ConvertInterfaceToFloat64(value interface{}) (float64, error) {
+    if value == nil {
+        return 0, nil
+    }
     nf := &sql.NullFloat64{}
     if err := nf.Scan(value); err != nil {
         return 0, err
@@ -38,6 +44,9 @@ func ConvertInterfaceToFloat64(value interface{}) (float64, error) {
 // Convert interface to int64.
 //////////////////////////////////////////////////////////////////////
 func ConvertInterfaceToInt64(value interface{}) (int64, error) {
+    if value == nil {
+        return 0, nil
+    }
     ni := &sql.NullInt64{}
     if err := ni.Scan(value); err != nil {
         return 0, err
@@ -50,6 +59,9 @@ func ConvertInterfaceToInt64(value interface{}) (int64, error) {
 // Convert interface to string.
 //////////////////////////////////////////////////////////////////////
 func ConvertInterfaceToString(value interface{}) (string, error) {
+    if value == nil {
+        return "", nil
+    }
     ns := &sql.NullString{}
     if err := ns.Scan(value); err != nil {
         return "", err
@@ -62,6 +74,9 @@ func ConvertInterfaceToString(value interface{}) (string, error) {
 // Convert interface to time.Time.
 //////////////////////////////////////////////////////////////////////
 func ConvertInterfaceToTime(value interface{}) (time.Time, error) {
+    if value == nil {
+        return time.Time{}, nil
+    }
     nt := &sql.NullTime{}
     if err := nt.Scan(value); err != nil {
         return time.Time{}, err
